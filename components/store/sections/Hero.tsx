6b2ec7abad0b3e2063174db049ca72a66110b310
Thejan Bandara <thejanbandara2@gmail.com>
2025-01-06 07:14:14 +0530

    added carousel img

--- components/store/sections/Hero.tsx
+++ components/store/sections/Hero.tsx
@@ -3,38 +3,39 @@
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
 import React from 'react'
 import Autoplay from 'embla-carousel-autoplay'
+import Image from 'next/image'
 
 const Hero = () => {
 
     const plugin = React.useRef(
-        Autoplay({delay: 5000, stopOnInteraction: false})
+        Autoplay({ delay: 5000, stopOnInteraction: false })
     )
 
-  return (
-    <section className='flex flex-col items-center justify-center mt-14'>
-        <Carousel
-        plugins={[plugin.current]}
-         className='w-full  p-2  bg-gray-100 lg:h-[50vh] lg:w-full aspect-video'>
-            <CarouselContent className='w-full h-full'>
-                <CarouselItem className='w-full p-2 flex items-center justify-center' key={1}>
-                    item 1
-                </CarouselItem>
-                <CarouselItem className='w-full h-full p-2' key={2}>
-                item 2
-                </CarouselItem>
+    return (
+        <section className='flex flex-col items-center justify-center mt-14'>
+            <Carousel
+                plugins={[plugin.current]}
+                className='w-full  p-2  bg-gray-100 lg:h-[50vh] lg:w-full aspect-video'>
+                <CarouselContent className='w-full h-full'>
+                    <CarouselItem className='w-full p-2 flex items-center justify-center' key={1} >
+                        <Image src={'/banner-bg-1.webp'} width={1200} height={1000} alt='bg' className='w-auto h-full overflow-hidden' />
+                    </CarouselItem>
+                    <CarouselItem className='w-full h-full p-2 flex items-center justify-center' key={2}>
+                        <Image src={'/banner-bg-2.webp'} width={1200} height={1000} alt='bg' className='w-auto h-full overflow-hidden' />
+                    </CarouselItem>
 
-                <CarouselItem className='w-full h-full p-2' key={3}>
-                item 3
-                </CarouselItem>
+                    <CarouselItem className='w-full h-full p-2 flex items-center justify-center' key={3}>
+                        <Image src={'/banner-bg-3.webp'} width={1200} height={1000} alt='bg' className='w-auto h-full overflow-hidden' />
+                    </CarouselItem>
 
-                <CarouselItem className='w-full h-full p-2' key={4}>
-                item 4
-                </CarouselItem>
+                    <CarouselItem className='w-full h-full p-2 flex items-center justify-center' key={4}>
+                    <Image src={'/banner-bg-4.webp'} width={1200} height={1000} alt='bg' className='w-auto h-full overflow-hidden'/>
+                    </CarouselItem>
 
-            </CarouselContent>            
-        </Carousel>
-    </section>
-  )
+                </CarouselContent>
+            </Carousel>
+        </section>
+    )
 }
 
 export default Hero
